a0e5c911fa17a86a0fad8563905f4ac2cc216393
Ippokratis Geranakis <igeran@cmt.local>
2024-04-30 11:46:07 +0300

    message

---
+++ models/Ekxorimena_TimologiaModel.js
@@ -0,0 +1,49 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Timologia from "./TimologiaModel.js";
+const {DataTypes} = Sequelize;
+
+const Ekxorimena_Timologia = db.define('Ekxorimena_Timologia',{
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    bank_ammount:
+    {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            isNumeric: true
+        }
+    },
+    customer_ammount:
+    {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            isNumeric: true
+        }
+    },
+    bank_date:
+    {
+        type: DataTypes.DATE,
+        allowNull: true,
+        validate: {
+            isDate: true
+        }
+    },
+    cust_date:
+    {
+        type: DataTypes.DATE,
+        allowNull: true,
+        validate: {
+            isDate: true
+        }
+    }
+},{
+    freezeTableName: true
+});
+
+Ekxorimena_Timologia.belongsTo(Timologia, { foreignKey: 'timologia_id', allowNull: true });
+export default Ekxorimena_Timologia;
