0cf1e1febb507017f05447a9880501c162c94e1c
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2024-05-22 18:01:41 +0300

    message

--- controllers/Queries.js
+++ controllers/Queries.js
@@ -2,7 +2,7 @@ import e from "express";
 //import User from "../models/UserModel.js";
 import argon2 from "argon2";
 import db from "../config/Database.js";
-import { Sequelize } from "sequelize";
+import {Op, Sequelize } from "sequelize";
 import Erga from "../models/ErgaModel.js";
 import Customer from "../models/CustomerModel.js";
 import Paradotea from "../models/ParadoteaModel.js";
@@ -47,21 +47,65 @@ export const getParErgColor = async (req, res) => {
     }
 };
 
-// export const getDaneiaById = async(req,res)=>{
-//     try{
-//         const response = await Daneia.findOne({
-//             attributes:['id','name','ammount','status'],
-//             where:{
-//                 id:req.params.id
-//             }
-//         });
-//         res.status(200).json(response);
+export const getSumofChoosenTimologioById = async (req, res) => {
+    const timologiaId = req.params.id;
+    try {
+        const response = await Paradotea.findAll({
+            attributes: [
+                [Sequelize.fn('SUM', Sequelize.col('paradotea.ammount_total')), 'totalek']
+            ],
+            
+            where: {
+                timologia_id: timologiaId
+            },
+        });
 
-//     } catch (error){
-//         res.status(500).json({ msg:error.message });
-//     }
+        if (!response) {
+            return res.status(404).json({ msg: "Timologia not found" });
+        }
+
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+
+//Get a unique name list of Erga  that have paradotea
+export const getErgaforTimologia = async (req, res) => {
+    try {
+        const response = await Paradotea.findAll({
+            attributes: ['title'],
+            include: [{
+                model: Erga,
+                attributes: ['name', 'id'],
+                required: true // Ensures INNER JOIN
+            }],
+            where: {
+                timologia_id: null
+            },
+            group: ['Erga.name', 'Erga.id', 'Paradotea.title'] // Group by the attributes you select
+        });
+
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+};
+export const getParadoteaByErgoId = async(req,res)=>{
+    try{
+        const response = await Paradotea.findAll({
+            attributes:['id','part_number','title','delivery_date','percentage','erga_id','timologia_id','ammount','ammount_vat','ammount_total','estimate_payment_date','estimate_payment_date_2','estimate_payment_date_3'],
+            where:{
+                erga_id:req.params.id
+            }
+        });
+        res.status(200).json(response);
+
+    } catch (error){
+        res.status(500).json({ msg:error.message });
+    }
     
-// }
-
+}
 
 
