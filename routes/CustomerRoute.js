9f81963429f34d2d80be9e8f09ac9023dd65a22b
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2024-09-20 12:18:50 +0300

    message

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -7,13 +7,14 @@ import {
     deleteCustomer
 } from "../controllers/Customer.js"
 import { verifyUser,adminOnly } from "../middleware/AuthUser.js";
+import { upload } from "../middleware/multer-config.js";  // Import multer config
 
 const router = express.Router();
 
 router.get('/customer',verifyUser,adminOnly, getCustomer);
 router.get('/customer/:id',verifyUser,adminOnly,getCustomerById);
-router.post('/customer',verifyUser,adminOnly,createCustomer);
-router.patch('/customer/:id',verifyUser,adminOnly,updateCustomer);
+router.post('/customer',upload.single('logoImage'),verifyUser,adminOnly,createCustomer);
+router.patch('/customer/:id',verifyUser,adminOnly,upload.single('logoImage'),updateCustomer);
 router.delete('/customer/:id',verifyUser,adminOnly,deleteCustomer);
 
 
--- routes/ErgaRoute.js
+++ routes/ErgaRoute.js
@@ -1,27 +1,3 @@
-// import express from "express";
-// import {
-//     getErga,
-//     getErgaById,
-//     createErga,
-//     updateErga,
-//     deleteErga,
-// } from "../controllers/Erga.js"
-// import { verifyUser,adminOnly } from "../middleware/AuthUser.js";
-// import { upload } from "../middleware/multer-config.js";  // Import multer config
-
-// const router = express.Router();
-
-// router.get('/erga',verifyUser,adminOnly, getErga);
-// router.get('/erga/:id',verifyUser,adminOnly,getErgaById);
-
-// //logo image uploade
-// router.post('/erga',verifyUser,adminOnly,createErga);
-// router.patch('/erga/:id',verifyUser,adminOnly,updateErga);
-// router.delete('/erga/:id',verifyUser,adminOnly,deleteErga);
-
-
-// export default router;
-
 import express from "express";
 import {
     getErga,
